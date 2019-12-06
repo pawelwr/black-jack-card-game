a3c190418bb5f871edf7e0d7b8b8358bd6fcb2c6
pawelwr <pawelwr1995@gmail.com>
2019-12-06 09:50:35 +0000

    delete os.environ...

--- config.py
+++ config.py
@@ -4,6 +4,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'blackjack'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
